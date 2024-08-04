97b6f2e28b641e936220a95a11c251dcdc45a013
dwkeis <dwkeis@gmail.com>
2024-08-05 00:41:39 +0800

    feat: index animation;

--- app/src/pages/IndexPage.vue
+++ app/src/pages/IndexPage.vue
@@ -62,32 +62,30 @@
 <script setup>
 import ThemePicker from "src/components/ThemePicker.vue";
 import ContentPicker from "src/components/ContentPicker.vue";
-import { onMounted, onUnmounted } from "vue";
+import { computed, onMounted, onUnmounted, shallowRef, watch } from "vue";
 import { ref } from "vue";
+import { useThemeStore } from "src/stores/theme";
 import * as THREE from "three";
-import { AsciiEffect } from "src/components/AsciiEffect.js";
+import { gsap } from "gsap";
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js";
+
+import { RGBShiftShader } from "three/addons/shaders/RGBShiftShader.js";
+import { DotScreenShader } from "three/addons/shaders/DotScreenShader.js";
+import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
 
 defineOptions({
   name: "IndexPage",
 });
 
+const myTheme = useThemeStore();
 const preLoad = ref(true);
 const currentPage = ref("Home");
 const homeClass = ref("d:flex");
 const bannerClass = ref("translateX(-60) opacity:0");
 const bannerTextClass = ref("translateX(90) opacity:0");
 const subTextClass = ref("opacity:0");
-
-// three
-const sizes = { width: 0, height: 0 };
-const scene = new THREE.Scene();
-const canvas = ref(null);
-const renderer = ref(null);
-const effect = ref(null);
-const effect1 = ref(null);
-const effect2 = ref(null);
-const effect3 = ref(null);
-const camera = ref(null);
 
 const isHome = (val) => {
   let previous = currentPage.value;
@@ -121,96 +119,180 @@ const isHome = (val) => {
   currentPage.value = val;
 };
 
-const initThree = async () => {
-  canvas.value = document.getElementById("webgl");
-  let size = document.getElementById("index_frame");
-  sizes.width = size.offsetWidth;
-  sizes.height = size.offsetHeight - 2;
-  camera.value = new THREE.PerspectiveCamera(
-    50,
-    sizes.width / sizes.height,
-    1,
-    1000
-  );
-  camera.value.position.set(10, 10, 40);
+// Use shallowRef for Three.js objects
+const scene = shallowRef(new THREE.Scene());
+const camera = shallowRef(null);
+const renderer = shallowRef(null);
+const composer = shallowRef(null);
+const pointLight1 = shallowRef(null);
+const pointLight2 = shallowRef(null);
+const currentColor = shallowRef(new THREE.Color(0xffffff));
+const backgroundColor = computed(() => {
+  return myTheme.getLightTheme ? 0xffffff : 0x000000;
+});
+let animationFrameId = null;
 
-  const HemisphereLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
-  scene.add(HemisphereLight);
+function createLight(color) {
+  const intensity = 1500;
 
-  const pointLight1 = new THREE.PointLight(0xff0040, 1, 20, 0);
-  pointLight1.power = 400;
-  scene.add(pointLight1);
+  const light = new THREE.PointLight(color, intensity, 200);
+  light.castShadow = true;
 
-  const pointLight2 = new THREE.PointLight(0x0040ff, 1, 20, 0);
-  pointLight2.power = 200;
-  scene.add(pointLight2);
+  // light bulb for debug usage
+  // let geometry = new THREE.SphereGeometry(0.3, 12, 6);
+  // let material = new THREE.MeshBasicMaterial({ color: color });
+  // material.color.multiplyScalar(intensity);
+  // let sphere = new THREE.Mesh(geometry, material);
+  // light.add(sphere);
+
+  // outside sphere
+  const texture = new THREE.CanvasTexture(generateTexture());
+  texture.magFilter = THREE.NearestFilter;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.repeat.set(1, 4.5);
+
+  let geometry = new THREE.SphereGeometry(2, 32, 8);
+  let material = new THREE.MeshPhongMaterial({
+    side: THREE.DoubleSide,
+    alphaMap: texture,
+    alphaTest: 0.5,
+    opacity: 0,
+  });
+
+  let sphere = new THREE.Mesh(geometry, material);
+  sphere.castShadow = true;
+  sphere.receiveShadow = false;
+  light.add(sphere);
+
+  return light;
+}
+
+function generateTexture() {
+  const canvas = document.createElement("canvas");
+  canvas.width = 2;
+  canvas.height = 2;
+
+  const context = canvas.getContext("2d");
+  context.fillStyle = "white";
+  context.fillRect(0, 1, 2, 1);
+
+  return canvas;
+}
+
+const initThree = () => {
+  const canvas = document.getElementById("webgl");
+  const size = document.getElementById("index_frame");
+  const width = size.offsetWidth;
+  const height = size.offsetHeight - 2;
+
+  camera.value = new THREE.PerspectiveCamera(50, width / height, 1, 1000);
+  camera.value.position.set(10, 5, 25);
+
+  scene.value.fog = new THREE.Fog(0x000000, 1, 1000);
+
+  const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
+  scene.value.add(hemisphereLight);
+
+  pointLight1.value = createLight(0x0088ff);
+  scene.value.add(pointLight1.value);
+
+  pointLight2.value = new THREE.PointLight(0x0040ff, 1, 20, 0);
+  pointLight2.value.power = 100;
+  scene.value.add(pointLight2.value);
 
   const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(20, 320, 160),
     new THREE.MeshPhongMaterial({
-      // color: 0xffffff,
-      // side: THREE.DoubleSide,
-      // alphaMap: texture,
-      // alphaTest: 0.5,
+      transparent: true,
+      opacity: 0.15,
+      side: THREE.BackSide,
+      shininess: 10,
     })
   );
-  sphere.castShadow = false; // Enable shadow casting
+  sphere.castShadow = false;
   sphere.receiveShadow = true;
-  scene.add(sphere);
+  scene.value.add(sphere);
 
-  // setRender
+  renderer.value = new THREE.WebGLRenderer({ canvas });
+  renderer.value.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.value.setSize(width, height);
+  renderer.value.shadowMap.enabled = true;
+  renderer.value.shadowMap.type = THREE.BasicShadowMap;
+  renderer.value.setClearColor(backgroundColor.value, 1);
 
-  renderer.value = new THREE.WebGLRenderer({
-    canvas: canvas.value,
-    alpha: true,
-  });
-  renderer.value.setSize(sizes.width, sizes.height);
+  composer.value = new EffectComposer(renderer.value);
+  composer.value.addPass(new RenderPass(scene.value, camera.value));
 
-  // effect.value = new AsciiEffect(renderer.value, "       .∙#", {
-  //   // origin value ' .:-+*=%@#'
-  //   invert: true,
-  // });
+  const effect1 = new ShaderPass(DotScreenShader);
+  effect1.uniforms["scale"].value = 4;
+  composer.value.addPass(effect1);
 
-  const animateLights = () => {
-    const time = Date.now() * 0.0007; // Current time in seconds
+  const effect2 = new ShaderPass(RGBShiftShader);
+  effect2.uniforms["amount"].value = 0.0015;
+  composer.value.addPass(effect2);
 
-    pointLight1.position.x = Math.sin(time * 0.7) * 30;
-    pointLight1.position.y = Math.cos(time * 0.5) * 30;
-    pointLight1.position.z = (Math.cos(time * 0.8) / 2) * 10;
+  const effect3 = new OutputPass();
+  composer.value.addPass(effect3);
 
-    pointLight2.position.x = Math.cos(time * 0.7) * 30;
-    pointLight2.position.y = Math.sin(time * 0.5) * 30;
-    pointLight2.position.z = (Math.sin(time * 0.8) / 2) * 30;
+  animate();
+};
 
-    // Render the scene
-    renderer.value.render(scene, camera.value);
-    // effect.value.render(scene, camera.value);
+const animate = () => {
+  const time = Date.now() * 0.0007; // Current time in seconds
+  pointLight1.value.position.y = Math.cos(time * 0.5) * 20;
+  pointLight1.value.rotation.x = time;
+  pointLight1.value.rotation.y = time;
 
-    // Call animateLights again on the next frame
-    requestAnimationFrame(animateLights);
-  };
-  animateLights();
-
-  // renderer.value.setAnimationLoop(animateLights);
-  // effect.value.setSize(sizes.width, sizes.height);
-  // effect.value.domElement.style.color = "black";
-  // effect.value.domElement.style.background = "white";
-  // document.getElementById("test_project").appendChild(effect.value.domElement);
+  pointLight2.value.position.x = Math.cos(time * 0.7) * 30;
+  pointLight2.value.position.y = Math.sin(time * 0.5) * 30;
+  pointLight2.value.position.z = (Math.sin(time * 0.8) / 2) * 30;
+  if (composer.value && scene.value && camera.value) {
+    scene.value.updateMatrixWorld();
+    composer.value.render();
+  }
+  animationFrameId = requestAnimationFrame(animate);
 };
 
 const onResize = () => {
-  canvas.value = document.getElementById("index_frame");
-  sizes.width = Math.max(1, Math.floor(canvas.value.offsetWidth));
-  sizes.height = Math.max(1, Math.floor(canvas.value.offsetHeight - 2));
+  if (!camera.value || !renderer.value) return;
 
-  camera.value.aspect = sizes.width / sizes.height;
+  const canvas = document.getElementById("index_frame");
+  const width = Math.max(1, Math.floor(canvas.offsetWidth));
+  const height = Math.max(1, Math.floor(canvas.offsetHeight - 2));
+
+  camera.value.aspect = width / height;
   camera.value.updateProjectionMatrix();
 
-  renderer.value.setSize(sizes.width, sizes.height);
+  renderer.value.setSize(width, height);
   renderer.value.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-  // effect.value.setSize(sizes.width, sizes.height);
+  if (composer.value) {
+    composer.value.setSize(width, height);
+  }
 };
+
+const transitionBackgroundColor = (targetColor) => {
+  if (scene.value) {
+    gsap.to(currentColor.value, {
+      r: targetColor.r,
+      g: targetColor.g,
+      b: targetColor.b,
+      duration: 1, // Duration of the transition in seconds
+      onUpdate: () => {
+        scene.value.background = currentColor.value;
+        scene.value.fog.color.copy(currentColor.value);
+      },
+    });
+  }
+};
+
+watch(backgroundColor, (newColor) => {
+  if (renderer.value) {
+    const targetColor = new THREE.Color(newColor);
+    transitionBackgroundColor(targetColor);
+  }
+});
 
 onMounted(() => {
   setTimeout(() => {
@@ -226,6 +308,9 @@ onMounted(() => {
 
 onUnmounted(() => {
   window.removeEventListener("resize", onResize);
+  if (animationFrameId) {
+    cancelAnimationFrame(animationFrameId);
+  }
 });
 </script>
 <style scoped>
